0b5790c19a2e5ba4a2a2e567ba2fc54cfaf4798e
Jack Dempsey <jdempsey1993@gmail.com>
2019-09-07 16:08:42 -0400

    Updated sql table

--- bamazon.sql
+++ bamazon.sql
@@ -4,13 +4,15 @@ USE bamazon;
 
 CREATE TABLE products(
 
-item_id INTEGER (10),
+item_id INT NOT NULL AUTO_INCREMENT,
 
-product_name VARCHAR (45),
+product_name VARCHAR (100) NOT NULL,
 
-department_name VARCHAR (45),
+department_name VARCHAR (100) NOT NULL,
 
-price INTEGER(10),
+price INTEGER(100) NOT NULL,
 
-stock_quantity INTEGER(10)
+stock_quantity INTEGER(100) NOT NULL
 ); 
+
+
