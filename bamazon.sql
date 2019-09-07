2f33e4bf5b4360f9e5f330c5c7dfbc9399caaaae
Jack Dempsey <jdempsey1993@gmail.com>
2019-09-07 15:51:41 -0400

    Generated mysql files, bamazon table

---
+++ bamazon.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+
+id INTEGER (10);
+
+productName VARCHAR (45);
+
+department_name VARCHAR (45);
+
+price INTEGER(10);
+
+stock_quantity(10)
+); 
