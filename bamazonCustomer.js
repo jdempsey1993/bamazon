5a4a23a12f0068a791e6e5aad08b8eb2eafbc8cc
Jack Dempsey <jdempsey1993@gmail.com>
2019-09-25 17:31:09 -0400

    Added function to bamazoncustomer.js to check stock quanitity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -60,4 +60,37 @@ function postId() {
         message: "What is the number of units you would like to buy?"
       }
     ])
+    .then(function(answer){
+      
+      console.log(answer)
+
+      //Define choices
+      
+      var choiceItemName = answer.action.toLowerCase()
+
+      var choiceItemQuantity = answer.value
+
+      var choiceItem
+      
+      for (let i =0; i < res.length; i++) {
+        item = res[i]
+
+        if (item.product_name === choiceItemName){
+          choiceItem = item
+        }
+        
+      } console.log (choiceItem)
+
+      if (choiceItem.stock_quantity>= choiceItemQuantity) {
+        console.log (choiceItemQuantity + " " +choiceItem.product_name+ "sold!")
+        
+        console.log (choiceItem.stock_quantity-choiceItemQuantity)
+
+        /// Update stock and process order
+        connection.query("UPDATE products SET stock_quantity=") + (chosenItem.stock_quantity - choiceItemQuantity)
+      } else { console.log ("Not enough stock, choose a small quanitity of"+choiceItem.product_name)
+      start()
+    }
+    }
+  )
 }
